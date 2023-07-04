6880a2ac06c679e34bde17db5c38cbf782568bd7
marysanders <lyremary@yahoo.com>
2023-07-03 21:40:15 -0500

    Complete task 4

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,67 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.Job;
+import org.launchcode.techjobs.oo.Employer;
+import org.launchcode.techjobs.oo.Location;
+import org.launchcode.techjobs.oo.PositionType;
+import org.launchcode.techjobs.oo.CoreCompetency;
+
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+    @Test
+    public void testSettingJobId() {
+        Job jobOne = new Job();
+        Job jobTwo = new Job();
+
+        assertNotEquals(jobOne.getId(), jobTwo.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+
+        Job jobThree = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+
+        assertTrue(jobThree instanceof Job);
+        assertTrue(jobThree.getEmployer() instanceof Employer);
+        assertTrue(jobThree.getLocation() instanceof Location);
+        assertTrue(jobThree.getPositionType() instanceof PositionType);
+        assertTrue(jobThree.getCoreCompetency() instanceof CoreCompetency);
+
+        assertEquals("Product tester", jobThree.getName());
+        assertEquals("ACME", jobThree.getEmployer().getValue());
+        assertEquals("Desert", jobThree.getLocation().getValue());
+        assertEquals("Quality control", jobThree.getPositionType().getValue());
+        assertEquals("Persistence", jobThree.getCoreCompetency().getValue());
+
+    }
+
+    @Test
+    public void testJobsForEquality() {
+
+        Job jobA = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+
+        Job jobB = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+
+        assertNotEquals(jobA, jobB);
+
+    }
 
 }
